fe2f858a3c47087c82096f8c1b11f3f634febbdf
Dainius <dainius.vanagas@codeacademy.lt>
2023-05-01 21:41:02 +0300

    First commit

---
+++ src/main/java/ca/BaigiamsisFE/BaigiamsisFeApplication.java
@@ -0,0 +1,13 @@
+package ca.BaigiamsisFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamsisFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamsisFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/BaigiamsisFE/BaigiamsisFeApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.BaigiamsisFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigiamsisFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
